731c16017bf20b869cc2764fbe72805c1776d450
mofblaster <mastero9017@gmail.com>
2017-02-12 17:55:26 +0100

    Prevent blocking from Airly

--- app/src/main/java/com/air/check/MainActivity.java
+++ app/src/main/java/com/air/check/MainActivity.java
@@ -164,7 +164,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     void printResult(String Latitude, String Longitude) {
         try{
-            String result = JsonTask("https://airapi.airly.eu/v1/mapPoint/measurements?latitude=" + Latitude + "&longitude=" + Longitude + "&apikey=0d23d883ef6a4689b938fa0dbf21e8f3");
+            //My apikey
+            // String apikey = "0d23d883ef6a4689b938fa0dbf21e8f3";
+            //Airly apikey
+            String apikey = "fae55480ef384880871f8b40e77bbef9";
+            String result = JsonTask("https://airapi.airly.eu/v1/mapPoint/measurements?latitude=" + Latitude + "&longitude=" + Longitude + "&apikey=" + apikey);
             JSONObject obj = new JSONObject(result);
 
             // Get info
